1904f3018704378a248832e09aff621e7637266b
RohanNaithani01 <rohannaithani02@gmail.com>
2026-05-16 10:12:16 +0530

    SelectionSort

--- src/main/java/org/example/SelectionSort/Main.java
+++ src/main/java/org/example/SelectionSort/Main.java
@@ -1,5 +1,30 @@
 package org.example.SelectionSort;
 
 public class Main {
+    public static void selection_sort(int[] arr, int n){
+        for(int i=0;i<=n-2;i++){
+            int mini=i;
+            for(int j =i;j<=n-1;j++){
+                if(arr[j]<arr[mini]){
+                    mini=j;
+                }
+            }
+            int temp=arr[mini];
+            arr[mini]=arr[i];
+            arr[i]=temp;
+        }
+
+    }
+    public static void main(String[] args) {
+        int[] arr={13,46,24,52,20,9};
+        int n=arr.length;
+
+        selection_sort(arr,n);
+        for(int i:arr){
+            System.out.println(i);
+        }
+
+    }
+
 
 }
